6ec6daffeaeda1da7178ee871cd9505dbcf32903
shashishekhar <fantabulousshashi@gmail.com>
2021-06-30 12:04:06 +0530

    All intended features working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql');
 
-app.get("/", (req, res)=>{
-    res.send("Hello World!")
-})
+const db = mysql.createPool({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "shashi",
+    connectionLimit: 5,
+    database: "crud_database",
+    debug: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res) => {
+
+    const sqlSelect = "SELECT * FROM movie_reviews";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post("/api/insert", (req, res) => {
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+    console.log(movieName);
+    const sqlInsert = "INSERT INTO movie_reviews (movie_name, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(err);
+    });
+});
 
 app.listen(3001, () => {
     console.log("running on port 3001");
--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  
+  // console.log("Trying to Post a request !!!!");
+  
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      console.log(response);
+    })
+  }, [])
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert",
+       { "movieName": movieName,
+        "movieReview": review
+      })
+  };
+  
   return (
     <div className="App">
       <h1>Your Movie Feed !</h1>
       <div className="form">
         <label>Movie Name</label>
-        <input type="text" name="movieName" />
+        <input type="text"
+               name="movieName" onChange={(e)=> {
+               setMovieName(e.target.value)
+              }}/>
         <label>Review</label>
-        <input type="text" name="review" />
-        <button>Submit</button>
+        <input type="text" 
+              name="review" onChange={(e)=> {
+              setReview(e.target.value)
+              }}/>
+        <button onClick={submitReview}>Submit</button>
+        {/* <button onClick={() => { console.log("button clicked");}}>button</button> */}
       </div>  
     </div>
   );
